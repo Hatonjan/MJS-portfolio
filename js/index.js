cc456cc683c273069af104f98e858181acd8a574
Jonathan <mj.scot.r@gmail.com>
2026-04-09 16:15:07 -0700

    Completed the 'Featured Projects' section

--- js/index.js
+++ js/index.js
@@ -1,2 +1,59 @@
 /* Updates the footer to the current year */
 document.getElementById("year").textContent = new Date().getFullYear(); 
+
+/* Render the feature projects section */
+async function featuredProjects() {
+    try {
+        // Fetch the JSON file
+        const response = await fetch('./assets/data/projects.json');
+        const data = await response.json(); 
+        
+        // Filter the first 4 featured projects
+        const featured = data.projects
+            .filter(p => p.featured === true)
+            .sort((a, b) => a.order - b.order)
+            .slice(0, 4);
+    
+        const html = featured.map(project => `
+            <div class="project-box">
+                <figure>
+                    <img class="thumbnail" src="${project.image}" alt="${project.altText}">
+                </figure>
+
+                <div class="project-box-line"></div>
+                
+                <p><strong>${project.title}: </strong>${project.description}</p>
+                
+                <div class="project-box-line"></div>
+                
+                <p><strong>Technologies:</strong> ${project.techStack.join(', ')}</p>
+                
+                <div class="project-box-line"></div>
+
+                <div class="cta-row">
+                    ${project.repoUrl ? `
+                        <a href="${project.repoUrl}" target="_blank">
+                            <figure>
+                                <img class="repo-icon" src="./assets/img/github-repo.png" alt="Github repo icon">
+                            </figure>
+                        </a>
+                    ` : `
+                        <p class="private-repo">PRIVATE REPO</p>
+                    `}                    
+                <a href="${project.liveUrl}" target="_blank">LIVE VIEW</a>
+                </div>
+            </div>
+        `).join('');
+
+        // Update the DOM
+        document.getElementById('project-container').innerHTML = html;
+    
+    // Error handling
+    } catch (error) {
+        console.error('Error loading projects: ', error);
+    }
+}
+
+// Call the function when page loads
+document.addEventListener('DOMContentLoaded', featuredProjects);
+/* End of featured projects*/
